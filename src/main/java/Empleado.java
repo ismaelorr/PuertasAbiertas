b0b0941980e9dbf138482113775c42ff5da90ffd
ismaelor03 <xxismaelor03xx@gmail.com>
2022-05-30 09:49:39 +0200

    first commit

---
+++ src/main/java/Empleado.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empleado {
+
+    private int cod_emple;
+    private String nombre;
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado();
+        Empleado empleado1 = new Empleado(1,"a");
+    }
+}
